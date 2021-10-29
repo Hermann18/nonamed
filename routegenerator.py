abbe595177a5e88687c4535430b0095e24d5a5c3
Hermann Karelian <77057575+Hermann18@users.noreply.github.com>
2021-10-29 12:03:13 +0300

    Delete routegenerator.py

--- routegenerator.py
+++
@@ -1,9 +0,0 @@
-from random import randint
-f = open("route", "w")
-new = ""
-for i in range(randint(100,150)):
-    for i in range(359):
-        new += f"{randint(2,7)},"
-    new += f"{randint(2,7)}"
-    new += "; \n"
-f.write(new)
