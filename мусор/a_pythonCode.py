ac122383677ad0f90e7eee3bebaca5a36c955302
Hermann18 <german.nesteruk.06@gmail.com>
2021-10-30 20:25:07 +0300

    v3

--- мусор/a_pythonCode.py
+++ мусор/a_pythonCode.py
@@ -1,4 +1,6 @@
+import time
 print("начало.работы")
+
 
 coordinatesActual = 0   #переменная отвечает за координаты в у.е. 
 
@@ -13,15 +15,12 @@ def findValue(what):    #для поиска определенных значе
 
 print("\n_____________")
 
-open("selfDataBase", "w").write("")
-open("outputToComputer", "w").write("")
-
-route_actual = open("route", "r").read().split(";")         #маршрут
+route_actual = open("route", "r").read().split(";")
 coordinatesMax = int(findValue("coordinatesMax"))           #максимальная длина выезда
-normalRadius =int(findValue("normalRadius"))                #нормальный радиус, получаем из приложения ( поскольку приложения нет - из файла)
-normalDelta = int(findValue("normalDelta"))                #нормальное отклонение радиуса, задаем в приложении ( поскольку приложения нет - в файле)
-lidarsCoordinates = [0, 90, 180, 270]                       #где Лидары
-print(coordinatesMax, normalRadius, normalDelta)            #проверяем
+normalRadius =int(findValue("normalRadius"))          #нормальный радиус, получаем из приложения ( поскольку приложения нет - из файла)
+normalDelta =  int(findValue("normalDelta"))             #нормальное отклонение радиуса, задаем в приложении ( поскольку приложения нет - в файле)
+coord_lidar = [0, 90, 180, 270]
+print(coordinatesMax, normalRadius, normalDelta)     #проверяем
 
 issues = [] #проблемки
 
@@ -32,47 +31,42 @@ def diamat(): # функция анализа окружающей среды
     degrees_result = []
     for i in range(360):
         degrees_result.append("x")
-    for i in range(len(lidarsCoordinates)):
-        for x in range(45):
-            lidarsCoordinates[i] = (lidarsCoordinates[i] + 1) % 360
-            degrees_result[lidarsCoordinates[i]] = int(route_actual[coordinatesActual].split(",")[lidarsCoordinates[i]])
-            print(lidarsCoordinates[i])
-
-    for u in range(len(degrees_result)):
-        if degrees_result[u] != "x" and normalRadius - degrees_result[u] > normalDelta:
-            issues_new.append([coordinatesActual, u])
+    for i in coord_lidar:     
+        for x in range(1, 43):
+            degrees_result[i+x] = int(route_actual[coordinatesActual].split(",")[i + x])
+            degrees_result[i-x] = int(route_actual[coordinatesActual].split(",")[i - x])
+        for u in range(len(degrees_result)):
+            if normalRadius - degrees_result[u] > normalDelta:
+                issues_new.append(coordinates_actual, u)
 
     for i in issues_new: #Обновляем файлики с проблемками записанными на самом устройстве
         issues.append(i) 
         k = open("selfDataBase", "r").read() + f"{i},"
         open("selfDataBase", "w").write( k )
-    
-    k = open("outputToComputer", "r").read() + f"{degrees_result}; \n"
-    open("outputToComputer", "w").write(k)
-
-def lazer(): #симуляция возможности приграды которую не проехать
-    if randint.random(0,100) < 10:
-        return False 
+    k = open("outputToComputer", "r").read() + degrees_result
+    open("outputToComputer", "w").write( k )
 
 def back():
     global coordinatesActual
     while coordinatesActual > 0:
         doStep(-1)
-        #print(coordinatesActual)
+        time.sleep(1)
 
 def doStep(vector):
     global coordinatesActual
+    print(vector)
     if coordinatesActual + (vector * 1) <= coordinatesMax and coordinatesActual + (vector * 1) >= 0:
         coordinatesActual = coordinatesActual + (vector * 1)
+    for i in coord_lidar:
+        i = (i + 45)%360
 
 while (stillWorking):
-    #print(coordinatesActual) 
-    if coordinatesActual < coordinatesMax and lazer():
+    print(coordinatesActual) 
+    if coordinatesActual < coordinatesMax:
         doStep(1)   #проверяем, можно ли делать шаг, делаем.шаг
         diamat()
     else:  #если нет - возвращаемся обратно (ездой назад)
         back()
-        stillWorking = False
    
             
 print("путь закончен");
