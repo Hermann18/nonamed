6fdb03965c39ce931ccef2e3a7d77e9fe48fe611
Hermann Karelian <77057575+Hermann18@users.noreply.github.com>
2021-10-31 10:17:41 +0300

    Update a_pythonCode.py

--- a_pythonCode.py
+++ a_pythonCode.py
@@ -1,80 +1,52 @@
-print("начало.работы")
+rint("начало работы")
+from random import randint
+import threading
 
+video = 1
+
+f = open("route", "w")
 coordinatesActual = 0   #переменная отвечает за координаты в у.е. 
+EMA_Degrees = [0,120,240]
+powers = [1,1,1]
 
-stillWorking = True     #ну очевидно
 
-def findValue(what):    #для поиска определенных значений из файла с "настрйками"
-    settings = open("inputFromComputer", "r").read()
+def diamat():
+    degrees = []
+    for y in range(360):
+        degrees.append("x")
+    for i in range(len(EMA_Degrees)):
+        for x in range(120):
+            EMA_Degrees[i] = (EMA_Degrees[i] + 1) % 360
+            degrees[EMA_Degrees[i]] = randint(3,6)
+    gyroscope = [randint(0, 359), randint(0, 359), randint(0, 359)]
+    sr = f":{degrees}=\n={gyroscope}; \n"
+    f = open("route", "r").read();
+    open("route", "w").write(f"{f}{sr}")
 
-    n = settings[ settings.find(what) + len(what) + 2: ]
-    # print( n[:n.find(";")], f"-{what}")
-    return n[:n.find(";")]  
-
-print("\n_____________")
-
-open("selfDataBase", "w").write("")
-open("outputToComputer", "w").write("")
-
-route_actual = open("route", "r").read().split(";")         #маршрут
-coordinatesMax = int(findValue("coordinatesMax"))           #максимальная длина выезда
-normalRadius =int(findValue("normalRadius"))                #нормальный радиус, получаем из приложения ( поскольку приложения нет - из файла)
-normalDelta = int(findValue("normalDelta"))                #нормальное отклонение радиуса, задаем в приложении ( поскольку приложения нет - в файле)
-lidarsCoordinates = [0, 90, 180, 270]                       #где Лидары
-print(coordinatesMax, normalRadius, normalDelta)            #проверяем
-
-issues = [] #проблемки
-
-def diamat(): # функция анализа окружающей среды
-    global coordinatesActual
-    issues_new = []
-
-    degrees_result = []
-    for i in range(360):
-        degrees_result.append("x")
-    for i in range(len(lidarsCoordinates)):
-        for x in range(45):
-            lidarsCoordinates[i] = (lidarsCoordinates[i] + 1) % 360
-            degrees_result[lidarsCoordinates[i]] = int(route_actual[coordinatesActual].split(",")[lidarsCoordinates[i]])
-
-    for u in range(len(degrees_result)):
-        if degrees_result[u] != "x" and normalRadius - degrees_result[u] > normalDelta:
-            issues_new.append([coordinatesActual, u])
-
-    for i in issues_new: #Обновляем файлики с проблемками записанными на самом устройстве
-        issues.append(i) 
-        k = open("selfDataBase", "r").read() + f"{i},"
-        open("selfDataBase", "w").write( k )
-    
-    k = open("outputToComputer", "r").read() + f"{degrees_result}; \n"
-    open("outputToComputer", "w").write(k)
-
-def lazer(): #симуляция возможности приграды которую не проехать
-    if randint.random(0,100) < 10:
-        return False 
-
-def back():
-    global coordinatesActual
-    while coordinatesActual > 0:
-        doStep(-1)
-        #print(coordinatesActual)
-
-def doStep(vector):
-    global coordinatesActual
-    if coordinatesActual + (vector * 1) <= coordinatesMax and coordinatesActual + (vector * 1) >= 0:
-        coordinatesActual = coordinatesActual + (vector * 1)
-
-while (stillWorking):
-    #print(coordinatesActual) 
-    if coordinatesActual < coordinatesMax and lazer():
-        doStep(1)   #проверяем, можно ли делать шаг, делаем.шаг
+def first_thread():
+    global coordinatesActual, video
+    print("ситстемы в норме")
+    while True:
         diamat()
-    else:  #если нет - возвращаемся обратно (ездой назад)
-        back()
-        stillWorking = False
-   
-            
-print("путь закончен");
+        lazers = [randint(0,100),randint(0,100,),randint(0,100,)]
+        if lazers[0] < 20 and lazers[1] < 20 and lazers[2] < 20:
+            print("vse")
+            video = 0
+            break
+        if lazers[0] != lazers[1] or lazers[1] != lazers[2] or lazers[0] != lazers[2]:
+            for i in range(len(lazers)):
+                if lazers[i] == max(lazers):
+                    powers[i] = 0.2
+                else:
+                    powers[i] = 1
+        coordinatesActual += 1
 
+def second_thread():
+    global video
+    while video:
+      print("запись видео") 
+first = threading.Thread(target = first_thread, name = "a")
+second = threading.Thread(target = second_thread, name = "c")
+first.start()
+second.start()
 
-# Отдельным потоком делаем передачу видео. Не знаю как оформить
