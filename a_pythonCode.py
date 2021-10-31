d78a6c60c857eef977e74c02707f20812dd7d0c8
Hermann Karelian <77057575+Hermann18@users.noreply.github.com>
2021-10-31 10:38:37 +0300

    Update a_pythonCode.py

--- a_pythonCode.py
+++ a_pythonCode.py
@@ -2,51 +2,52 @@ print("начало работы")
 from random import randint
 import threading
 
-video = 1
+video = 1 # переменная отвечаеет за запись видео
 
-f = open("route", "w")
+f = open("route", "w") #очищаем файлик с маршрутом
 coordinatesActual = 0   #переменная отвечает за координаты в у.е. 
-EMA_Degrees = [0,120,240]
-powers = [1,1,1]
+EMA_Degrees = [0,120,240] #градусы расположения датчиков
+powers = [1,1,1] #сила на колесах
 
 
-def diamat():
-    degrees = []
+def diamat(): #функция записи данных от датчиков
+    degrees = [] 
     for y in range(360):
-        degrees.append("x")
-    for i in range(len(EMA_Degrees)):
+        degrees.append("x") #для удобства
+    for i in range(len(EMA_Degrees)): 
         for x in range(120):
-            EMA_Degrees[i] = (EMA_Degrees[i] + 1) % 360
-            degrees[EMA_Degrees[i]] = randint(3,6)
-    gyroscope = [randint(0, 359), randint(0, 359), randint(0, 359)]
-    sr = f":{degrees}=\n={gyroscope}; \n"
+            EMA_Degrees[i] = (EMA_Degrees[i] + 1) % 360 #двигаем датчик на градус
+            degrees[EMA_Degrees[i]] = randint(3,6) #поскольку нет реальных данных, толщина стенки равна случайному значению от 3 до 6
+    gyroscope = [randint(0, 359), randint(0, 359), randint(0, 359)] #нет реальных данных - генерируем их для гироскопа
+    sr = f":{degrees}=\n={gyroscope}; \n" #создаем строку с получившимися данными
     f = open("route", "r").read();
-    open("route", "w").write(f"{f}{sr}")
+    open("route", "w").write(f"{f}{sr}") #записываем все в файлик route
 
 def first_thread():
     global coordinatesActual, video
     print("ситстемы в норме")
     while True:
         diamat()
-        lazers = [randint(0,100),randint(0,100,),randint(0,100,)]
-        if lazers[0] < 20 and lazers[1] < 20 and lazers[2] < 20:
-            print("vse")
-            video = 0
-            break
-        if lazers[0] != lazers[1] or lazers[1] != lazers[2] or lazers[0] != lazers[2]:
-            for i in range(len(lazers)):
-                if lazers[i] == max(lazers):
-                    powers[i] = 0.2
+        distanceMeters = [randint(0,100),randint(0,100,),randint(0,100,)] #симулируем значения датчиков дальнометра
+        if distanceMeters[0] < 20 and distanceMeters[1] < 20 and distanceMeters[2] < 20: #если слишком близко 
+            video = 0 #выключаем видео
+            break #завершаем работу
+        if distanceMeters[0] != distanceMeters[1] or distanceMeters[1] != distanceMeters[2] or distanceMeters[0] != distanceMeters[2]: #если показания различны
+            for i in range(len(distanceMeters)):
+                if distanceMeters[i] == max(distanceMeters): 
+                    powers[i] = 0.2 #уменьшаем скорость колеса, если его датчик показал самое большое расстояние
                 else:
                     powers[i] = 1
-        coordinatesActual += 1
+        else:
+            for i in range (len(powers)): #восстанавливаем нормальные значения мощности колес
+                powers[i] = 1 
+        coordinatesActual += 1 #условно шаг вперед
 
 def second_thread():
     global video
     while video:
-      print("запись видео") 
+        pass #записываем видео пока video == 1
 first = threading.Thread(target = first_thread, name = "a")
 second = threading.Thread(target = second_thread, name = "c")
 first.start()
 second.start()
-
